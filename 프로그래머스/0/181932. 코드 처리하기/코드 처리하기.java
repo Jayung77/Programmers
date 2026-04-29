d682d03ce15c9603a9eee095b169a5e35c10bada
JayJeong <121883047+Jayung77@users.noreply.github.com>
2026-04-29 16:38:07 +0900

    Refactor code handling logic and comments

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -3,10 +3,8 @@ class Solution {
     public String solution(String code) {
         String answer = "";
         int mode = 0;
-       char[] cod = code.toCharArray(); 
-        
-        StringBuilder ret = new StringBuilder(); 
-        
+       char[] cod = code.toCharArray(); // String(문자열)을 한 글자씩 쪼개서 char 배열(문자 배열)로 변환하는 함수, cod[i]처럼 인덱스로 접근가능
+        StringBuilder ret = new StringBuilder(); //글자들을 이어 붙여서 하나의 완성된 문자열을 만들기 위한 '가변 문자열 저장소' 생성  
         for(int i=0; i<code.length(); i++){
             if(mode==0){
                 if (cod[i] == '1'){
@@ -26,6 +24,6 @@ class Solution {
                 }
             }
         }
-        return ret.length() == 0 ? "EMPTY" : ret.toString(); 
+        return ret.length() == 0 ? "EMPTY" : ret.toString(); // 빌더 형태여서 문자열 형태로 바꿔서 반환
     }
 }
